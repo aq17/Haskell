665dea6dc71c3e111b647290fc7c42d4b79c39a1
aq17 <aqneco@gmail.com>
2019-02-03 00:44:36 +0900

    whereを用いたbmi関数

---
+++ Chapter3/where.hs
@@ -0,0 +1,14 @@
+main = do
+  print(bmiTell 54.9 1.715)
+
+bmiTell :: Double -> Double -> String
+bmiTell weight height
+  | bmi <= skinny = "You're underweight, you emo, you!"
+  | bmi <= normal = "You're supposedly normal.\
+                  \ Pffft, I bet you're ugly!"
+  | bmi <= fat = "You're fat! Lose some weight, fatty!"
+  | otherwise   = "You're a whale, congratulations!"
+  where bmi = weight / height ^ 2
+        skinny = 18.5
+        normal = 25.0
+        fat = 30.0
