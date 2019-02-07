1e15a2740ec93212f175efec6e55af0bb05beba6
aq17 <aqneco@gmail.com>
2019-02-07 20:37:20 +0900

    ジェネレータ(let)

--- Chapter3/let.hs
+++ Chapter3/let.hs
@@ -1,8 +1,12 @@
 main = do
     print(cylinder 3 5)
+    print(clacBmis [(54.1,1.71)])
 
 cylinder :: Double -> Double -> Double
 cylinder r h =
     let sideArea = 2 * pi * r * h
         topArea = pi * r ^ 2
     in sideArea + 2 * topArea
+
+clacBmis :: [(Double,Double)] -> [Double]
+clacBmis xs = [bmi | (w,h) <- xs, let bmi = w/h^2,bmi > 25.0]
