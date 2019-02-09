5f7f71630d0093bb4acb545a3fc08a9e5110e7b2
aq17 <aqneco@gmail.com>
2019-02-10 08:10:06 +0900

    takeWhile関数を使った実装

--- Chapter5/exMapFilter.hs
+++ Chapter5/exMapFilter.hs
@@ -1,5 +1,12 @@
 main = do
   print(largestDivisible)
+  let data1 = takeWhile (/=' ') "elephants know how to party"
+  --空白で区切った最初の単語を返す
+  print(data1)
+  let data2 = sum(takeWhile (<10000) (filter odd (map (^2) [1..])))
+  --10000より小さい全ての奇数の平方数の和
+  print(data2)
+
 
 largestDivisible :: Integer
 largestDivisible = head(filter p [100000,99999..])
