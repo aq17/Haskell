62916495eef6ac069182d005aa8a9b7054d78110
aq17 <aqneco@gmail.com>
2019-02-10 08:16:33 +0900

    コラッツ数列問題

--- Chapter5/exMapFilter.hs
+++ Chapter5/exMapFilter.hs
@@ -6,6 +6,7 @@ main = do
   let data2 = sum(takeWhile (<10000) (filter odd (map (^2) [1..])))
   --10000より小さい全ての奇数の平方数の和
   print(data2)
+  print(numLongChains)
 
 
 largestDivisible :: Integer
@@ -13,4 +14,14 @@ largestDivisible = head(filter p [100000,99999..])
 --100000以下の数をフィルタリング
   where p x = x `mod` 3829 == 0
   -- 3829 で割り切れる最初の数
+
+koratz :: Integer -> [Integer]
+koratz 1 = [1]
+koratz n
+  | even n = n : koratz (n `div` 2)
+  | odd n = n : koratz (n * 3 + 1)
+
+numLongChains :: Int
+numLongChains = length (filter isLong (map koratz [1..100]))
+  where isLong xs = length xs > 15
 
