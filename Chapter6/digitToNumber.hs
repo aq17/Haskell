1c6af08edfe47f891a37aaa3874b32c0daeee687
aq17 <aqneco@gmail.com>
2019-03-15 19:40:37 +0900

    Maybe型を使った型安全

---
+++ Chapter6/digitToNumber.hs
@@ -0,0 +1,15 @@
+import Data.Char
+import Data.List
+
+main = do
+  print firstTo40
+  print (firstTo 52)
+
+digitSum :: Int -> Int
+digitSum = sum . map digitToInt . show
+
+firstTo40 :: Maybe Int
+firstTo40 = find (\x -> digitSum x == 40) [1..]
+
+firstTo :: Int -> Maybe Int
+firstTo n = find (\x -> digitSum x == n) [1..]
