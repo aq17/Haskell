ef09ebff924e66a3bae8581001eea3e7f3b36907
aq17 <aqneco@gmail.com>
2019-03-15 21:14:16 +0900

    連想配列

---
+++ Chapter6/mapping.hs
@@ -0,0 +1,16 @@
+phoneBook = [("betty","555-2938")
+  ,("bonnie","452-2928")
+  ,("patsy","493-2928")
+  ,("lucille","205-2928")
+  ,("wendy","939-8282")
+  ,("penny","853-2492")
+  ]
+
+main = do
+  print (findKey "penny" phoneBook)
+
+findKey :: (Eq k) => k -> [(k,v)] -> Maybe v
+findKey key [] = Nothing
+findKey key ((k,v):xs)
+  | key == k = Just v
+  | otherwise = findKey key xs
