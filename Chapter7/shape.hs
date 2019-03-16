b5a8acbd70c26b9d1d4cc770d0d6848a92055f22
aq17 <aqneco@gmail.com>
2019-03-16 18:27:32 +0900

    図形の作成

--- Chapter7/shape.hs
+++ Chapter7/shape.hs
@@ -1,12 +1,19 @@
-data Shape = Circle Float Float Float |
-             Rectangle Float Float Float Float
-  deriving (Show)
+data Point = Point Float Float deriving (Show)
+data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
              
 area :: Shape -> Float
-area (Circle _ _ r) = pi * r ^ 2
-area (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
+area (Circle _ r) = pi * r ^ 2
+area (Rectangle (Point x1 y1)(Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
+
+nudge :: Shape -> Float -> Float -> Shape
+nudge (Circle (Point x y) r) a b = Circle (Point (x+a) (y+b)) r
+nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b = Rectangle (Point (x1+a) (y1+b)) (Point (x2+a) (y2+b))
+
+baseRect :: Float -> Float -> Shape
+baseRect width height = Rectangle (Point 0 0) (Point width height)
 
 main = do
-  print (Circle 10 20 5)
-  print (Rectangle 50 230 60 90)
-  print (map (Circle 10 20) [4,5,6,6])
+  print (area (Rectangle (Point 0 0) (Point 100 100)))
+  print (area (Circle (Point 0 0) 24))
+  print (nudge (Circle (Point 34 34) 10) 5 10)
+  print (nudge (baseRect 40 100) 60 23)
