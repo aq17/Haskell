a5f934e972fecc0e77df544e9e742898cf6e04bc
aq17 <aqneco@gmail.com>
2019-03-19 18:14:52 +0900

    演算子の定義

---
+++ Chapter7/tree.hs
@@ -0,0 +1,12 @@
+infixr 5 :-:
+data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
+
+infixr 5 ^++
+(^++) :: List a -> List a -> List a
+Empty ^++ ys = ys
+(x :-: xs) ^++ ys = x :-: (xs ^++ ys)
+
+main = do
+  let a = 3 :-: 4 :-: 5 :-: Empty
+  let b = 6 :-: 7 :-: Empty
+  print (a ^++ b)
