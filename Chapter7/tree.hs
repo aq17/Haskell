302d4019fc13d0db594228a2f4ba39bf61ea77bb
aq17 <aqneco@gmail.com>
2019-03-19 18:32:36 +0900

    木の生成

--- Chapter7/tree.hs
+++ Chapter7/tree.hs
@@ -6,7 +6,30 @@ infixr 5 ^++
 Empty ^++ ys = ys
 (x :-: xs) ^++ ys = x :-: (xs ^++ ys)
 
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+  | x == a = Node x left right
+  | x < a = Node a (treeInsert x left) right
+  | x > a = Node a left (treeInsert x right)
+  
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+  | x == a = True
+  | x < a  = treeElem x left
+  | x > a  = treeElem x right
+
 main = do
   let a = 3 :-: 4 :-: 5 :-: Empty
   let b = 6 :-: 7 :-: Empty
   print (a ^++ b)
+  
+  let nums = [8,6,4,1,7,3,5]
+  let numsTree = foldr treeInsert EmptyTree nums
+  print (numsTree)
