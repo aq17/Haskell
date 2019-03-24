37df60188fab5f0ad8e922892607d6dc35ea90f9
aq17 <aqneco@gmail.com>
2019-03-25 05:47:57 +0900

    演算

--- Haskell入門/Chapter2/integer.hs
+++ Haskell入門/Chapter2/integer.hs
@@ -10,3 +10,14 @@ main = do
   print (0o644::Int) -- 8進数表記
   print (1.0::Double) -- 浮動小数点型
   print (1.23e2::Double) -- 指数表記e
+  print (3.4028236e+38::Float) --桁あふれ
+  print (isNaN (3.4028236e+38::Float))
+  print (isInfinite (3.4028236e+38::Float))
+  print (0.0/0.0::Double) -- 不定値
+  print (isNaN (0.0/0.0::Double))
+  print (isInfinite (0.0/0.0::Double))
+  print (1 + 1 :: Int) -- 和算
+  print (2.3 * 3.4 :: Float) -- 乗算
+  print (3 - 13 :: Integer) -- 引き算
+  print (12 `div` 3 :: Int) -- 割り算
+  print (12.0 / 3.0 :: Double)
